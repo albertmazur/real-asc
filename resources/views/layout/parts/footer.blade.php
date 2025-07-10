d1e5611ec89d83abfe1a8e6be4ab0325f83a2c67
albertmazur <albert153@interia.pl>
2025-07-10 16:09:53 +0200

    style for nav in footer

--- resources/views/layout/parts/footer.blade.php
+++ resources/views/layout/parts/footer.blade.php
@@ -1,31 +1,28 @@
-<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-    <div class="col-md-4 d-flex align-items-center">
-        <a href="albertmazur.pl" target="_blank" class="mb-3 me-3 mb-md-0 text-body-secondary text-decoration-none lh-1">
-              <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
-        </a>
-        <a href="https://www.albertmazur.pl" target="_blank" class="link-secondary mb-3 mb-md-0" rel="nofollow" >© 2025 Albert Mazur</a>
-    </div>
-    <nav>
-        <a class="btn" href="{{ route('event.index') }}">{{ __('app.events') }}</a>
-        <a class="btn" href="{{ route('stadium.list') }}">{{ __('app.club_facilities') }}</a>
-        <a class="btn" href="{{ route('about') }}">{{ __('app.about.title') }}</a>
-        <a class="btn" href="{{ route('contact') }}">{{ __('app.contact.title') }}</a>
-    </nav>
-    <ul class="nav col-md-4 justify-content-end list-unstyled d-flex me-3">
-        <li class="ms-3">
-            <a href="https://x.com" target="_blank" class="icon text-body-secondary">
-                <i class="bi bi-twitter-x fs-3"></i>
-            </a>
-        </li>
-        <li class="ms-3">
-            <a href="https://www.instagram.com" target="_blank" class="icon text-body-secondary">
-                <i class="bi bi-instagram fs-3"></i>
-            </a>
-        </li>
-        <li class="ms-3">
-            <a href="https://www.facebook.com" target="_blank" class="icon text-body-secondary">
-                <i class="bi bi-facebook fs-3"></i>
-            </a>
-        </li>
-    </ul>
+<footer class="d-flex flex-column flex-md-row justify-content-between align-items-center py-3 border-top px-4">
+  <div class="d-flex align-items-center mb-3 mb-md-0">
+    <a href="https://albertmazur.pl" target="_blank" class="link-secondary text-decoration-none">© 2025 Albert Mazur</a>
+  </div>
+  <nav class="nav justify-content-center mb-3 mb-md-0">
+    <a class="nav-link px-2 text-body-secondary" href="{{ route('event.index') }}">{{ __('app.events') }}</a>
+    <a class="nav-link px-2 text-body-secondary" href="{{ route('stadium.list') }}">{{ __('app.club_facilities') }}</a>
+    <a class="nav-link px-2 text-body-secondary" href="{{ route('about') }}">{{ __('app.about.title') }}</a>
+    <a class="nav-link px-2 text-body-secondary" href="{{ route('contact') }}">{{ __('app.contact.title') }}</a>
+  </nav>
+  <ul class="list-unstyled d-flex mb-0">
+    <li class="ms-3">
+      <a href="https://x.com" target="_blank" class="text-body-secondary fs-4">
+        <i class="bi bi-twitter-x"></i>
+      </a>
+    </li>
+    <li class="ms-3">
+      <a href="https://www.instagram.com" target="_blank" class="text-body-secondary fs-4">
+        <i class="bi bi-instagram"></i>
+      </a>
+    </li>
+    <li class="ms-3">
+      <a href="https://www.facebook.com" target="_blank" class="text-body-secondary fs-4">
+        <i class="bi bi-facebook"></i>
+      </a>
+    </li>
+  </ul>
 </footer>
