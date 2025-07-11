0d4be78e644eff37112e3c2648ffbf90c7b9fc4c
albertmazur <albert153@interia.pl>
2025-07-11 14:24:28 +0200

    form search for event dashboard

--- resources/views/dashboard/admin/event/main.blade.php
+++ resources/views/dashboard/admin/event/main.blade.php
@@ -3,22 +3,7 @@
 @section('dashboard.content')
     @include('layout.parts.errors')
     <div class="shadow p-3 mb-5 bg-body-tertiary rounded">
-        <form action="{{ route('event.dashboard') }}" method="GET">
-            <div class="mb-3">
-                <label for="nameSearch" class="form-label">{{ __('app.find') }}</label>
-                <input type="text" id="nameSearch" name="nameSearch" class="form-control" placeholder="{{ __('app.enter_name') }}" value="{{ $nameSearch ?? '' }}">
-            </div>
-            <div class="mb-3">
-                <label for="sortSearch" class="form-label">{{ __('app.sort') }}</label>
-                <select name="sortSearch" class="form-select" aria-label="{{ __('dashboard.sort_choice') }}">
-                    <option @if($sortSearch == 'name') selected @endif value="name">{{ __('app.name') }}</option>
-                    <option @if($sortSearch == 'date') selected @endif value="date">{{ __('app.date') }}</option>
-                    <option @if($sortSearch == 'freeSet') selected @endif value="freeSet">{{ __('app.free_places') }}</option>
-                    <option @if($sortSearch == 'price') selected @endif value="price">{{ __('app.price') }}</option>
-                </select>
-            </div>
-            <button type="submit" class="btn btn-primary">{{ __('app.find') }}</button>
-        </form>
+        @include('event.form-search', ['route' => 'event.dashboard'])   
     </div>
     <div class="table-responsive">
         <table class="table table-striped text-center">
---
+++ resources/views/event/form-search.blade.php
@@ -0,0 +1,31 @@
+<form action="{{ route($route) }}" method="GET">
+            <div class="mb-3">
+                <label for="value" class="form-label">{{ __('app.find') }}</label>
+                <input type="text" id="value" name="value" class="form-control" placeholder="{{ __('app.enter_name') }}" value="{{ $value ?? '' }}">
+            </div>
+            <div class="mb-3">
+                <label for="stadium" class="form-label">{{ __('app.stadium') }}</label>
+                <select id="facility" name="facility" class="form-select" aria-label="{{ __('dashboard.sort_choice') }}">
+                    <option value="0" {{ $facility == 0 ? 'selected' : '' }}>{{ __('app.all') }}</option>
+                    @foreach ($stadiums as $stadium)
+                        <option value="{{ $stadium->id }}" {{ $facility == $stadium->id ? 'selected' : '' }}>{{ $stadium->name }}</option>
+                    @endforeach
+                </select>
+            </div>
+            <label for="sortSearch" class="form-label">{{ __('app.sort') }}</label>
+            <div class="mb-3">
+                <div class="input-group">
+                    <span class="input-group-text">{{ __('app.sort') }}</span>
+                    <select id="sortSearch" name="sortSearch" class="form-select" aria-label="{{ __('dashboard.sort_choice') }}">
+                        <option value="name" {{ $sortSearch == 'name' ? 'selected' : '' }}>{{ __('app.name') }}</option>
+                        <option value="date" {{ $sortSearch == 'date' ? 'selected' : '' }}>{{ __('app.date') }}</option>
+                        <option value="freeSet" {{ $sortSearch == 'freeSet'? 'selected' : '' }}>{{ __('app.free_places') }}</option>
+                    </select>
+                    <select id="sortDirection" name="sortDirection" class="form-select" aria-label="{{ __('app.sort_direction') }}">
+                        <option value="asc"  {{ $sortDirection == 'asc' ? 'selected' : '' }}>{{ __('app.asc') }}</option>
+                        <option value="desc" {{ $sortDirection == 'desc' ? 'selected' : '' }}>{{ __('app.desc') }}</option>
+                    </select>
+                </div>
+            </div>
+            <button type="submit" class="btn btn-primary">{{ __('app.find') }}</button>
+        </form>
