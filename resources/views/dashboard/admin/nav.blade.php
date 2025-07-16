935589cd75051104e923449cc227ba7085a1d4b5
albertmazur <albert153@interia.pl>
2025-07-16 10:55:51 +0200

    dashboard nav

--- resources/views/dashboard/admin/nav.blade.php
+++ resources/views/dashboard/admin/nav.blade.php
@@ -1,29 +1,19 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary mb-4 rounded">
-    <div class="container-fluid">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="{{ __('app.nav_toggle') }}">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link {{ request()->routeIs('user.*') ? 'active' : '' }}" href="{{ route('user.users') }}">{{ __('app.users') }}</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link {{ request()->routeIs('stadium.*') ? 'active' : '' }}" href="{{ route('stadium.index') }}">{{ __('app.stadiums') }}</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link {{ request()->routeIs('event.*') ? 'active' : '' }}" href="{{ route('event.dashboard') }}">{{ __('app.events') }}</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link {{ request()->routeIs('submission.*') ? 'active' : '' }}" href="{{ route('submission.index') }}">{{ __('app.submissions') }}</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link {{ request()->routeIs('comment.*') ? 'active' : '' }}" href="{{ route('comment.index') }}">{{ __('app.comments') }}</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link {{ request()->routeIs('comment.*') ? 'active' : '' }}" href="{{ route('ticket.scanner') }}">{{ __('app.ticket') }}</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
+<li class="nav-item">
+    <a class="nav-link {{ request()->routeIs('user.*') ? 'active' : '' }}" href="{{ route('user.users') }}">{{ __('app.users') }}</a>
+</li>
+<li class="nav-item">
+    <a class="nav-link {{ request()->routeIs('stadium.*') ? 'active' : '' }}" href="{{ route('stadium.index') }}">{{ __('app.stadiums') }}</a>
+</li>
+<li class="nav-item">
+    <a class="nav-link {{ request()->routeIs('event.*') ? 'active' : '' }}" href="{{ route('event.dashboard') }}">{{ __('app.events') }}</a>
+</li>
+<li class="nav-item">
+    <a class="nav-link {{ request()->routeIs('submission.*') ? 'active' : '' }}" href="{{ route('submission.index') }}">{{ __('app.submissions') }}</a>
+</li>
+<li class="nav-item">
+    <a class="nav-link {{ request()->routeIs('comment.*') ? 'active' : '' }}" href="{{ route('comment.index') }}">{{ __('app.comments') }}</a>
+</li>
+<li class="nav-item">
+    <a class="nav-link {{ request()->routeIs('comment.*') ? 'active' : '' }}" href="{{ route('ticket.scanner') }}">{{ __('app.ticket') }}</a>
+</li>
+
--- resources/views/dashboard/main.blade.php
+++ resources/views/dashboard/main.blade.php
@@ -2,10 +2,21 @@
 @extends('layout.app')
 
 @section('content')
-    @if(Auth::user()->role === UserRole::ADMIN->value) @include('dashboard.admin.nav')
-    @elseif(Auth::user()->role === UserRole::MODERATOR->value) @include('dashboard.moderator.nav')
-    @elseif(Auth::user()->role === UserRole::USER->value) @include('dashboard.client.nav')
-    @endif
+    <nav class="navbar navbar-expand-lg bg-body-tertiary mb-4 rounded">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="{{ __('app.nav_toggle') }}">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    @if(Auth::user()->role === UserRole::ADMIN->value) @include('dashboard.admin.nav')
+                    @elseif(Auth::user()->role === UserRole::MODERATOR->value) @include('dashboard.moderator.nav')
+                    @elseif(Auth::user()->role === UserRole::USER->value) @include('dashboard.client.nav')
+                    @endif
+                </ul>
+            </div>
+        </div>
+    </nav>
 
     @hasSection('dashboard.content')
         @yield('dashboard.content')
